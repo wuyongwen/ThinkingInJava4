da08184f94f9eff85a33388d4680e0d954ca43e3
wuyongwen <wuyongwen@gmail.com>
2015-09-01 02:19:30 +0800

    thingking in java 4

---
+++ net/mindview/util/OSExecuteDemo.java
@@ -0,0 +1,14 @@
+//: io/OSExecuteDemo.java
+package net.mindview.util;/* Added by Eclipse.py */
+
+
+public class OSExecuteDemo {
+	public static void main(String[] args) {
+		//OSExecute.command("javap ./bin/net.mindview.util.OSExecuteDemo");
+		OSExecute.command("cmd");
+	}
+} /*
+ * Output: Compiled from "OSExecuteDemo.java" public class OSExecuteDemo extends
+ * java.lang.Object{ public OSExecuteDemo(); public static void
+ * main(java.lang.String[]); }
+ */// :~
