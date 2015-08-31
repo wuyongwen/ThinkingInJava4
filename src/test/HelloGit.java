1a211af58a00b9e3d25609a1ad1e4f92739eb84a
wuyongwen <wuyongwen@gmail.com>
2015-09-01 01:40:40 +0800

    commit

--- src/test/HelloGit.java
+++ src/test/HelloGit.java
@@ -1,7 +1,11 @@
 package test;
 
+import java.util.Scanner;
+
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("hello gitHub!");
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextLine());
 	}
 }
